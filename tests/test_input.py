2f7568a9a48790433abdc12705d3ab2b1423f20d
Jordi Piriz <jpiriz@redhat.com>
2025-01-13 12:09:26 +0100

    Add helper methods for terraform based modules (#13)

--- tests/test_input.py
+++ tests/test_input.py
@@ -1,16 +1,28 @@
+import tempfile
 from typing import Any
 
 import pytest
+from pydantic import BaseModel
 
 from external_resources_io.input import (
     AppInterfaceProvision,
     TerraformProvisionOptions,
+    create_backend_tf_file,
+    create_tf_vars_json,
     parse_model,
 )
 
 
+class Data(BaseModel):
+    identifier: str
+    assume_role: dict[str, str | list[str] | None]
+    inline_policy: str
+    output_resource_name: str
+    region: str
+
+
 @pytest.fixture
-def data() -> dict[str, Any]:
+def ai_data() -> dict[str, Any]:
     return {
         "data": {
             "identifier": "test-external-resources-iam-role",
@@ -41,8 +53,33 @@ def data() -> dict[str, Any]:
     }
 
 
-def test_parse_provision(data: dict) -> None:
-    o = parse_model(AppInterfaceProvision, data["provision"])
-    assert isinstance(o, AppInterfaceProvision)
-    assert isinstance(o.module_provision_data, TerraformProvisionOptions)
-    assert o.module_provision_data.tf_state_region == "us-east-1"
+@pytest.fixture
+def provision_data(ai_data: dict[str, Any]) -> AppInterfaceProvision:
+    return parse_model(AppInterfaceProvision, ai_data["provision"])
+
+
+@pytest.fixture
+def data(ai_data: dict[str, Any]) -> Data:
+    return parse_model(Data, ai_data["data"])
+
+
+def test_parse_provision(provision_data: AppInterfaceProvision) -> None:
+    assert isinstance(provision_data, AppInterfaceProvision)
+    assert isinstance(provision_data.module_provision_data, TerraformProvisionOptions)
+    assert provision_data.module_provision_data.tf_state_region == "us-east-1"
+
+
+def test_tf_vars_json(data: Data) -> None:
+    with tempfile.NamedTemporaryFile(delete=True) as temp_file:
+        create_tf_vars_json(data, temp_file.name)
+        assert temp_file.read().decode("utf-8") == (
+            '{"identifier":"test-external-resources-iam-role","assume_role":{"aws":"null","service":["ec2.amazonaws.com"],"federated":"null"},"inline_policy":"{\\"Version\\":\\"2012-10-17\\",\\"Statement\\":[{\\"Effect\\":\\"Allow\\",\\"Action\\":[\\"ec2:DescribeVpcs\\"],\\"Resource\\":[\\"*\\"]}]}","output_resource_name":"test-external-resources","region":"us-east-1"}'
+        )
+
+
+def test_create_backaned_tf_file(provision_data: AppInterfaceProvision) -> None:
+    with tempfile.NamedTemporaryFile(delete=True) as temp_file:
+        create_backend_tf_file(provision_data, temp_file.name)
+        assert temp_file.read().decode("utf-8") == (
+            '\nterraform {\n  backend "s3" {\n    bucket = "test-external-resources-state"\n    key    = "aws/ter-int-dev/aws-iam-role/test-external-resources-iam-role/terraform.state"\n    region = "us-east-1"\n    dynamodb_table = "test-external-resources-lock"\n    profile = "external-resources-state"\n  }\n}\n'
+        )
