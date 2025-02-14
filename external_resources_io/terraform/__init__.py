d18442abee539144fd78d93acf680da63a61515b
Christian Assing <cassing@redhat.com>
2025-02-14 08:09:36 +0100

    export more public terraform methods

--- external_resources_io/terraform/__init__.py
+++ external_resources_io/terraform/__init__.py
@@ -2,13 +2,28 @@ from .generators import (
     create_backend_tf_file,
     create_tf_vars_json,
     create_variables_tf_file,
+    create_variables_tf_json_file,
 )
-from .plan import Plan, TerraformJsonPlanParser
+from .plan import (
+    Action,
+    Change,
+    DeferredResourceChange,
+    Plan,
+    ResourceAttribute,
+    ResourceChange,
+    TerraformJsonPlanParser,
+)
 
 __all__ = [
+    "Action",
+    "Change",
+    "DeferredResourceChange",
     "Plan",
+    "ResourceAttribute",
+    "ResourceChange",
     "TerraformJsonPlanParser",
     "create_backend_tf_file",
     "create_tf_vars_json",
     "create_variables_tf_file",
+    "create_variables_tf_json_file",
 ]
